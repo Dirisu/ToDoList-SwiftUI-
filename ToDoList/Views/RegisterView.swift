75f5e8276806c2d4a1ab54c87787c4cecbacd413
Dirisu <dirisumarvellous94@gmail.com>
2023-07-31 21:24:09 +0100

    create registerModel view

--- ToDoList/Views/RegisterView.swift
+++ ToDoList/Views/RegisterView.swift
@@ -9,9 +9,7 @@ import SwiftUI
 
 struct RegisterView: View {
     
-    @State var name = ""
-    @State var email = ""
-    @State var password = ""
+  @StateObject var viewModel = RegisterViewViewModel()
     
     var body: some View {
         
@@ -21,16 +19,16 @@ struct RegisterView: View {
             
             // REGISTERATION FORM
             Form {
-                TextField("Full Name", text: $name)
+                TextField("Full Name", text: $viewModel.name)
                     .textFieldStyle(DefaultTextFieldStyle())
                     .autocorrectionDisabled()
                 
-                TextField("Email Address", text: $email)
+                TextField("Email Address", text: $viewModel.email)
                     .textFieldStyle(DefaultTextFieldStyle())
                     .autocorrectionDisabled()
                     .textInputAutocapitalization(.never)
                 
-                SecureField("Password", text: $password)
+                SecureField("Password", text: $viewModel.password)
                     .textFieldStyle(DefaultTextFieldStyle())
                 
                 TLButtonView(title: "Create Account", background: .orange, action: {
